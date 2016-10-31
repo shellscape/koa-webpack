97e131b5fa9a83715b5c8c4cadf803d64c0d35da
Marcus Christensson <marcus.christensson@gmail.com>
2016-10-31 17:02:36 +0100

    Use webpack-dev-middleware function waitUntilValid to resolve promise (#3)
    * Use webpack-dev-middleware function waitUntilValid to resolve promise
    
    * Add compiler plugin failure handler again

--- index.js
+++ index.js
@@ -22,9 +22,8 @@ function koaDevware (compiler, options) {
 
   function middleware (context, next) {
     return new Promise((resolve, reject) => {
-      // https://github.com/webpack/docs/wiki/plugins#donestats-stats
-      compiler.plugin('done', (stats) => {
-        resolve(stats);
+      dev.waitUntilValid(() => {
+        resolve(true);
       });
 
       compiler.plugin('failed', (error) => {
