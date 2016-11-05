e2f17c59b4b8a625b16983dd778493d3a82bebe7
Paul Myburgh <paulmyburgh88@gmail.com>
2016-11-05 04:01:26 +0200

    Fixes #4- Resources requested outside of webpack bundle working now. (#5)
    * fixes #4- Resources requested outside of webpack bundle working now. Seperated out the valid Webpack bundle check from the usual middleware method to stop it resolving too early and not processing the request properly.
    
    * changes for linting and better naming. Also pushed version number to 0.1.8
    
    * reverted /dist to old version. undid package.json changes
    
    * reverted dist/index.js to exact old version so no changes should be made on pull request

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import * as path from 'path';
 function koaDevware (compiler, options) {
   const dev = devMiddleware(compiler, options);
 
-  function middleware (context, next) {
+  function waitUntilValidOrFailed() {
     return new Promise((resolve, reject) => {
       dev.waitUntilValid(() => {
         resolve(true);
@@ -18,18 +18,17 @@ function koaDevware (compiler, options) {
       compiler.plugin('failed', (error) => {
         reject(error);
       });
-
-      dev(context.req, {
-        end: (content) => {
-          context.body = content;
-        },
-        setHeader: context.set.bind(context)
-      }, next);
     });
   }
 
   return async (context, next) => {
-    await middleware(context, next);
+    await waitUntilValidOrFailed();
+    await dev(context.req, {
+      end: (content) => {
+        context.body = content;
+      },
+      setHeader: context.set.bind(context)
+    }, next);
   };
 }
 
