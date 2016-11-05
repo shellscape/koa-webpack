49a262f7a7a385bd3d146d315c5a27083de2d765
shellscape <andrew@shellscape.org>
2016-11-04 22:19:17 -0400

    fixes #4: v0.1.8

--- index.js
+++ index.js
@@ -6,10 +6,18 @@ import compose from 'koa-compose';
 import root from 'app-root-path';
 import * as path from 'path';
 
+/**
+ * @method koaDevware
+ * @desc   Middleware for Koa to proxy webpack-dev-middleware
+ **/
 function koaDevware (compiler, options) {
   const dev = devMiddleware(compiler, options);
 
-  function waitUntilValidOrFailed() {
+  /**
+   * @method waitMiddleware
+   * @desc   Provides blocking for the Webpack processes to complete.
+   **/
+  function waitMiddleware () {
     return new Promise((resolve, reject) => {
       dev.waitUntilValid(() => {
         resolve(true);
@@ -22,7 +30,7 @@ function koaDevware (compiler, options) {
   }
 
   return async (context, next) => {
-    await waitUntilValidOrFailed();
+    await waitMiddleware();
     await dev(context.req, {
       end: (content) => {
         context.body = content;
@@ -32,6 +40,10 @@ function koaDevware (compiler, options) {
   };
 }
 
+/**
+ * @method koaHotware
+ * @desc   Middleware for Koa to proxy webpack-hot-middleware
+ **/
 function koaHotware (compiler, options) {
   const hot = hotMiddleware(compiler, options);
 
@@ -49,6 +61,9 @@ function koaHotware (compiler, options) {
   };
 }
 
+/**
+ * The entry point for the Koa middleware.
+ **/
 export default (options) => {
 
   const defaults = { dev: {}, hot: {} };
