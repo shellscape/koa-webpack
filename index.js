d122ca1c86ac59d5fcc8308b64ab41599dbcee4c
Jan Möller <jan.moeller0@gmail.com>
2017-02-23 23:30:34 +0100

    Set locals field on the dev-middleware response argument (#31)

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ function koaDevware (dev, compiler) {
       end: (content) => {
         context.body = content;
       },
-      setHeader: context.set.bind(context)
+      setHeader: context.set.bind(context),
+      locals: context.state
     }, next);
   };
 }
