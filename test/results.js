55a160e53ce2499fe903b22e332bd5935c928fc7
Mike Marcacci <mike.marcacci@gmail.com>
2018-03-08 22:47:31 -0700

    add failing test for MultiCompiler

--- test/results.js
+++ test/results.js
@@ -90,6 +90,42 @@ describe('koa-webpack', () => {
       });
   }).timeout(5e3);
 
+  it('sends the result to a MultiCompiler in watch mode', (done) => {
+    const { middleware, req, server } = setup({
+      dev: { lazy: false },
+      config: [
+        {
+          entry: [path.resolve(__dirname, 'fixtures', 'input.js')],
+          output: {
+            path: path.resolve(__dirname, 'fixtures'),
+            filename: 'output.js'
+          }
+        },
+        {
+          entry: [path.resolve(__dirname, 'fixtures', 'input.js')],
+          output: {
+            path: path.resolve(__dirname, 'fixtures'),
+            filename: 'output2.js'
+          }
+        }
+      ]
+    });
+    req.get('/output.js')
+      .expect(200)
+      .expect((response) => {
+        assert.ok(/Hello world/.test(response.text), "Expected result to contain 'Hello world'");
+      })
+      .then(() => req.get('/output2.js')
+        .expect(200)
+        .expect((response) => {
+          assert.ok(/Hello world/.test(response.text), "Expected result to contain 'Hello world'");
+        })
+        .then(() => {
+          server.kill();
+          middleware.close(done);
+        }));
+  }).timeout(5e3);
+
   it('builds and sends the result in lazy mode', (done) => {
     const { middleware, req, server } = setup({ dev: { lazy: true } });
     req.get('/output.js')
